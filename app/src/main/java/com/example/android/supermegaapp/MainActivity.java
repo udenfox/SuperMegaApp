c243c8c432ed356cb1affcd751487bbd8afe0b52
Hajba <hajba.vova@gmail.com>
2019-07-09 13:30:08 +0300

    VladimirHaiba class added

--- app/src/main/java/com/example/android/supermegaapp/MainActivity.java
+++ app/src/main/java/com/example/android/supermegaapp/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
+        ValadimirHaiba haiba = new ValadimirHaiba("Vladimir","Ivanovich");
 
         FloatingActionButton fab = findViewById(R.id.fab);
 
